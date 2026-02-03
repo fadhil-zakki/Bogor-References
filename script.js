aac4a4eab9f5535762972ec86c716f486259ea48
Fadhil Zakki Zulva <168079274+fadhil-zakki@users.noreply.github.com>
2026-02-03 19:59:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const toggle = document.querySelector('.menu-toggle');
 const menu = document.getElementById('menu');
 
-if (toggle) {
-  toggle.addEventListener('click', () => {
-    menu.classList.toggle('show');
-  });
-}
+toggle.addEventListener('click', () => {
+  menu.classList.toggle('show');
+});
