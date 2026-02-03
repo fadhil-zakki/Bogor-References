c5de9595cfb44dbaa2aa2360a5a674fa05b0d3e6
Fadhil Zakki Zulva <168079274+fadhil-zakki@users.noreply.github.com>
2026-02-03 20:19:30 +0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const toggle = document.querySelector('.menu-toggle');
-const menu = document.getElementById('menu');
-
-toggle.addEventListener('click', () => {
-  menu.classList.toggle('show');
-});
