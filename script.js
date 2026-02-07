a806dc68eaddb2f090b8dab36c6bf77fbe3dd06f
Fadhil Zakki Zulva <168079274+fadhil-zakki@users.noreply.github.com>
2026-02-08 05:56:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ function toggleMenu() {
   document.getElementById('menu').classList.toggle('active');
 }
 
-// active nav
+// active nav + close menu on click
 document.querySelectorAll('.nav-menu a').forEach(link => {
   if (link.href === window.location.href) {
     link.classList.add('active');
   }
+
+  link.addEventListener('click', () => {
+    document.getElementById('menu').classList.remove('active');
+  });
 });
